af1a3398ea8d5c06633f1353901aae36aa0526ab
Gabby <GabbyVByers@gmail.com>
2025-09-07 21:32:48 -0700

    folks we love the heap

--- perceptron.h
+++ perceptron.h
@@ -6,15 +6,29 @@ class Perceptron
 {
 public:
 
+	int numLayers = -1;
 	const std::vector<int> networkStructure = { 784, 16, 16, 10 };
 
-	float* inputActivations = nullptr;
-	float** networkActivations = nullptr;
-	float** networkBises = nullptr;
-	float*** networkWeights = nullptr;
+	float*** weights = nullptr;
+	float** biases = nullptr;
+	float** activations = nullptr;
+	float** z_values = nullptr;
+	float** dCdz = nullptr;
+	float*** dCdw = nullptr;
+	float* expectedValues = nullptr;
 
 	Perceptron()
 	{
-
+		numLayers = networkStructure.size();
+		mallocNetwork();
 	}
+
+	~Perceptron()
+	{
+		freeNetwork();
+	}
+
+	void mallocNetwork();
+	void freeNetwork();
 };
+
---
+++ perceptron_memory.cpp
@@ -0,0 +1,69 @@
+
+#include "perceptron.h"
+
+void Perceptron::mallocNetwork()
+{
+	biases = new float* [numLayers];
+	activations = new float* [numLayers];
+	z_values = new float* [numLayers];
+	dCdz = new float* [numLayers];
+	weights = new float** [numLayers];
+	dCdw = new float** [numLayers];
+	expectedValues = new float[networkStructure[numLayers - 1]];
+
+	for (int L = 1; L < numLayers; L++)
+	{
+		int J = networkStructure[L];
+		int K = networkStructure[L - 1];
+		weights[L] = new float* [J];
+		dCdw[L] = new float* [J];
+		for (int j = 0; j < J; j++)
+		{
+			weights[L][j] = new float[K];
+			dCdw[L][j] = new float[K];
+		}
+
+		biases[L] = new float[J];
+		z_values[L] = new float[J];
+		dCdz[L] = new float[J];
+	}
+
+	for (int L = 0; L < numLayers; L++)
+	{
+		int J = networkStructure[L];
+		activations[L] = new float[J];
+	}
+}
+
+void Perceptron::freeNetwork()
+{
+	for (int L = 1; L < numLayers; L++)
+	{
+		int J = networkStructure[L];
+		for (int j = 0; j < J; j++)
+		{
+			delete[] weights[L][j];
+			delete[] dCdw[L][j];
+		}
+
+		delete[] weights[L];
+		delete[] dCdw[L];
+		delete[] biases[L];
+		delete[] z_values[L];
+		delete[] dCdz[L];
+	}
+	delete[] weights;
+	delete[] dCdw;
+
+	for (int L = 0; L < numLayers; L++)
+	{
+		delete[] activations[L];
+	}
+
+	delete[] biases;
+	delete[] activations;
+	delete[] z_values;
+	delete[] dCdz;
+	delete[] expectedValues;
+}
+
